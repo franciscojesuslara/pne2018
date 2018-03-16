3fb26ac989d69b5fabe6ae5feab83859dffad7da
fj.lara.2017 <franje99@hotmail.es>
2018-03-16 16:16:00 +0100

    initial upload of newfile.py

---
+++ openfda/newfile.py
@@ -0,0 +1,11 @@
+a=1
+b=0
+print("hello from Pycharm 0")
+print("hello from Pycharm 1")
+print("hello from Pycharm 2")
+
+print("hello from Pycharm 3")
+print("hello from Pycharm 4")
+print("hello from Pycharm 5")
+print("hello from Pycharm 6")
+print("hello from Pycharm 7")
